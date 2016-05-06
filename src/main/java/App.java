3bd659a588d21b42978c776bdb0e21521a66f407
Tommy Jones <tommyinseason@yahoo.com>
2016-05-06 09:53:04 -0700

    Passing AppTest rootTest

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,7 @@
-import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -11,29 +9,10 @@ public class App {
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("wordList", request.session().attribute("wordList"));
+      HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    post("/words", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-
-      ArrayList<Word> words = request.session().attribute("words");
-        if (words == null) {
-          words = new ArrayList<Word>();
-          request.session().attribute("words", words);
-    }
-
-      String userInput = request.queryParams("userInputWord");
-      Word newWord = new Word(userInput);
-      words.add(newWord);
-
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,7 +3,6 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
 
@@ -17,38 +16,12 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
-  // 
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Dictionary");
-  // }
-  //
-  // @Test
-  // public void WordIsCreatedTest() {
-  //   goTo("http://localhost:4567/");
-  //   fill("#userInputWord").with("apple");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Your word has been saved.");
-  // }
-  // @Test
-  // public void taskIsDisplayedTest() {
-  //   goTo("http://localhost:4567/");
-  //   fill("#userInputWord").with("apple");
-  //   submit(".btn");
-  //   click("a", withText("Go Back"));
-  //   assertThat(pageSource()).contains("apple");
-  // }
-  // @Test
-  // public void multipleTasksAreDisplayedTest() {
-  //   goTo("http://localhost:4567/");
-  //   fill("#userInputWord").with("apple");
-  //   submit(".btn");
-  //   click("a", withText("Go Back"));
-  //   fill("#userInputWord").with("bananas");
-  //   submit(".btn");
-  //   click("a", withText("Go Back"));
-  //   assertThat(pageSource()).contains("apple");
-  //   assertThat(pageSource()).contains("bananas");
-  // }
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Dictionary!");
+    assertThat(pageSource()).contains("View Word List");
+    assertThat(pageSource()).contains("Add a New Word");
+  }
 }
